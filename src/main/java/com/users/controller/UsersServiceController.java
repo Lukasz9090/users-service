405652a24214dd3ae03851d03ded779ab6c2a678
Łukasz Soska <lukasz.soskaa@gmail.com>
2026-01-03 11:44:22 +0100

    controller change

--- src/main/java/com/users/controller/UsersServiceController.java
+++ src/main/java/com/users/controller/UsersServiceController.java
@@ -2,22 +2,35 @@ package com.users.controller;
 
 import com.users.model.UserDto;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.webmvc.autoconfigure.WebMvcProperties;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j2
 @RestController
 @RequestMapping("/users")
 public class UsersServiceController {
 
-    @GetMapping()
-    public ResponseEntity<UserDto> getUsers() {
+    private static List<UserDto> users = new ArrayList<>();
 
+    static {
+        users.add(new UserDto("1", "John Doe"));
+    }
+
+    @GetMapping()
+    public ResponseEntity<List<UserDto>> getUsers() {
         log.info("Getting users");
-        var user = new UserDto("1", "John Doe");
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @PostMapping()
+    public ResponseEntity<UserDto> addUser(@RequestBody UserDto user) {
+        log.info("Adding user: {}", user);
+        users.add(user);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
 
